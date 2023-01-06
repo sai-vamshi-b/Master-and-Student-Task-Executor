db08d9312212659b6b88f38ef70d4fdf3722f816
Sai Vamshi Boggula <saivamshi0103@gmail.com>
2023-01-06 22:33:06 +0530

    ver.06.01.2023

--- main/views.py
+++ main/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-# Create your views here.
-def home(request):
+from .models import Task
+
+class TaskList(ListView):
+	model = Task
+
+def homepage(request):
 	return render(request,"main/index.html")
 
 def signup(request):
-
 	if request.method == "POST":
-
 		username = request.POST['username']
 		firstname = request.POST['firstname']
 		lastname = request.POST['lastname']
@@ -20,29 +23,22 @@ def signup(request):
 		myuser = User.objects.create_user(username, email, pass1)
 		myuser.first_name = firstname
 		myuser.last_name = lastname
-
 		myuser.save()
-		
 		messages.success(request, "Your account has been created!")
-
 		return redirect('signin')
 
 	return render(request,"main/signup.html")
 
 def signin(request):
-
 	if request.method == "POST":
-
 		username = request.POST['username']
 		pass1 = request.POST['pass1']
 
 		user = authenticate(username=username, password=pass1)
-
 		if user is not None:
 			login(request, user)
 			firstname = user.first_name
 			return render(request, "main/index.html", {'firstname':firstname})
-
 		else:
 			messages.error(request, "Bad credentials!")
 			return redirect('home')
